798e7a6e240bba0f396aae9911ea03c70cb69dea
Hasan Bilal <hasanmbilal1998@gmail.com>
2023-03-09 10:00:39 +0300

    try fix issues

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function createFile (){
     }catch(error){
       core.setFailed("couldn't create directory structure");
     }
-    await fs.writeFile(path.join(absolutePath), yamlStr, 'utf8')
+    await fs.writeFile(path.join(absolutePath, file+'.yaml'), yamlStr, 'utf8')
   }catch (error) {
     core.setFailed(error.message);
   }
